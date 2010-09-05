6c23af0a9c1d1923433f1670e4c8591e39b7132d
Michael Chaten <mchaten@gmail.com>
2010-09-05 17:06:13 -0400

    Fix cmd queue compile

--- DOpenCL/command_queue.d
+++ DOpenCL/command_queue.d
@@ -14,17 +14,15 @@
   limitations under the License.
 */
 import DOpenCL.raw;
-import DOpenCL.device_id;
-import DOpenCL.context;
 struct CommandQueue {
   cl_command_queue _queue;
   alias _queue this;
   this(cl_command_queue queue) {
     _queue = queue;
   }
-  this(Context context,DeviceID device_id,cl_command_queue_properties properties) {
+  this(cl_context context,cl_device_id device_id,cl_command_queue_properties properties) {
     cl_int err_code;
-    _queue = clCreateCommandQueue(context.context,device_id.device_id,properties,&err_code);
+    _queue = clCreateCommandQueue(context,device_id,properties,&err_code);
     assert(err_code == CL_SUCCESS);
   }
   ~this() {
