c7dfaa7f4a28fad0e4e75e9338d319750a283c30
Michael Chaten <mchaten@gmail.com>
2010-09-05 20:23:33 -0400

    Tests, bug fixes

--- DOpenCL/device_id.d
+++ DOpenCL/device_id.d
@@ -21,5 +21,125 @@ struct DeviceID {
   this(cl_device_id my_device_id) {
     _device_id = my_device_id;
   }
+  private {
+    T device_scalar_info(T)(cl_device_info param_name) {
+      T param_value;
+      auto err_code = clGetDeviceInfo(this,param_name,T.sizeof,&param_value,null);
+      assert(err_code == CL_SUCCESS);
+      return param_value;
+    }
+    T[] device_array_info(T)(cl_device_info param_name) {
+      T[] param_value;
+      size_t param_value_size_ret;
+      auto err_code = clGetDeviceInfo(this,param_name,T.sizeof,null,&param_value_size_ret);
+      assert(err_code == CL_SUCCESS);
+      param_value = new T[param_value_size_ret];
+      err_code = clGetDeviceInfo(this,param_name,T.sizeof * param_value_size_ret,param_value.ptr,null);
+      assert(err_code == CL_SUCCESS);
+      return param_value;
+    }
+  }
+  cl_uint address_bits() {
+    cl_uint ret;
+    auto err_code = clGetDeviceInfo(this,CL_DEVICE_ADDRESS_BITS,ret.sizeof,&ret,null);
+    assert(CL_SUCCESS == err_code);
+    return ret;
+  }
+  bool available() {
+    cl_bool ret;
+    auto err_code = clGetDeviceInfo(this,CL_DEVICE_AVAILABLE,ret.sizeof,&ret,null);
+    assert(CL_SUCCESS == err_code);
+    return ret == CL_TRUE;
+  }
+  bool compiler_available() {
+    cl_bool ret;
+    auto err_code = clGetDeviceInfo(this,CL_DEVICE_COMPILER_AVAILABLE,ret.sizeof,&ret,null);
+    assert(CL_SUCCESS == err_code);
+    return ret == CL_TRUE;
+  }/*
+  cl_device_fp_config double_fp_config() {
+    cl_device_fp_config ret;
+    auto err_code = clGetDeviceInfo(this,CL_DEVICE_DOUBLE_FP_CONFIG,ret.sizeof,&ret,null);
+    assert(CL_SUCCESS == err_code);
+    return ret;
+  }*/
+  bool is_little_endian() {
+    cl_bool ret;
+    auto err_code = clGetDeviceInfo(this,CL_DEVICE_ENDIAN_LITTLE,ret.sizeof,&ret,null);
+    assert(CL_SUCCESS == err_code);
+    return ret == CL_TRUE;
+  }
+  bool has_ecc() {
+    cl_bool ret;
+    auto err_code = clGetDeviceInfo(this,CL_DEVICE_ERROR_CORRECTION_SUPPORT,ret.sizeof,&ret,null);
+    assert(CL_SUCCESS == err_code);
+    return ret == CL_TRUE;
+  }
+  cl_device_exec_capabilities execution_capabilities() {
+    cl_device_exec_capabilities ret;
+    auto err_code = clGetDeviceInfo(this,CL_DEVICE_EXECUTION_CAPABILITIES,ret.sizeof,&ret,null);
+    assert(CL_SUCCESS == err_code);
+    return ret;
+  }
+  string extensions() {
+    char [] extensions;
+    size_t extensions_len;
+    auto err_code = clGetDeviceInfo(this,CL_DEVICE_EXTENSIONS,extensions.sizeof,null,&extensions_len);
+    assert(CL_SUCCESS == err_code);
+    extensions = new char[extensions_len];
+    err_code = clGetDeviceInfo(this,CL_DEVICE_EXTENSIONS,char.sizeof*extensions_len,extensions.ptr,null);
+    assert(CL_SUCCESS == err_code);
+    return cast(immutable)extensions;
+  }
+  cl_ulong global_mem_cache_size() {
+    cl_ulong ret;
+    auto err_code = clGetDeviceInfo(this,CL_DEVICE_GLOBAL_MEM_CACHE_SIZE,ret.sizeof,&ret,null);
+    assert(err_code == CL_SUCCESS);
+    return ret;
+  }
+  cl_device_mem_cache_type global_mem_cache_type() {
+    cl_device_mem_cache_type type;
+    auto err_code = clGetDeviceInfo(this,CL_DEVICE_GLOBAL_MEM_CACHE_TYPE,type.sizeof,&type,null);
+    assert(err_code == CL_SUCCESS);
+    return type;
+  }
+  cl_uint global_mem_cacheline_size() {
+    cl_uint ret;
+    auto err_code = clGetDeviceInfo(this,CL_DEVICE_GLOBAL_MEM_CACHELINE_SIZE,ret.sizeof,&ret,null);
+    assert(err_code == CL_SUCCESS);
+    return ret;
+  }
+  cl_ulong global_mem_size() {
+    cl_ulong ret;
+    auto err_code = clGetDeviceInfo(this,CL_DEVICE_GLOBAL_MEM_SIZE,ret.sizeof,&ret,null);
+    assert(err_code == CL_SUCCESS);
+    return ret;
+  }
+ /* cl_device_fp_config half_fp_config() {
+    cl_device_fp_config ret;
+    auto err_code = clGetDeviceInfo(this,CL_DEVICE_HALF_FP_CONFIG,ret.sizeof,&ret,null);
+    assert(err_code == CL_SUCCESS);
+    return ret;
+  } */
+  bool has_image_support() {
+    cl_bool ret;
+    auto err_code = clGetDeviceInfo(this,CL_DEVICE_IMAGE_SUPPORT,ret.sizeof,&ret,null);
+    assert(err_code == CL_SUCCESS);
+    return ret == CL_TRUE;
+  }
+  //TODO: Image height,width,ect
+  cl_ulong local_mem_size() {
+    cl_ulong ret;
+    auto err_code = clGetDeviceInfo(this,CL_DEVICE_LOCAL_MEM_SIZE,ret.sizeof,&ret,null);
+    assert(err_code == CL_SUCCESS);
+    return ret;
+  }
+  cl_device_local_mem_type local_mem_type() {
+    cl_device_local_mem_type ret;
+    auto err_code = clGetDeviceInfo(this,CL_DEVICE_LOCAL_MEM_TYPE,ret.sizeof,&ret,null);
+    assert(err_code == CL_SUCCESS);
+    return ret;
+  }
+
   /* TODO: Implement get device info */
 }
---
+++ DOpenCL/platform_id.d
@@ -0,0 +1,102 @@
+/*
+  Copyright 2010 Michael Chaten
+        
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and 
+  limitations under the License.
+*/
+import DOpenCL.raw;
+import DOpenCL.device_id;
+import std.algorithm;
+struct PlatformID {
+  cl_platform_id _id;
+  alias _id this;
+  this(cl_platform_id id) {
+    _id = id;
+  }
+  private {
+    string get_str_info(cl_platform_info info) {
+      char [] ptr;
+      size_t ptr_size;
+      auto err_code = clGetPlatformInfo(this,info,ptr.sizeof,null,&ptr_size);
+      assert(err_code != CL_INVALID_VALUE);
+      assert(err_code != CL_INVALID_PLATFORM);
+      assert(err_code == CL_SUCCESS);
+      ptr = new char[ptr_size];
+      err_code = clGetPlatformInfo(this,info,ptr.sizeof*ptr_size,ptr.ptr,null);
+      assert(err_code == CL_SUCCESS);
+      return cast(immutable)ptr;
+    }
+    DeviceID [] get_devices(cl_device_type type) {
+      cl_uint num_devices;
+      cl_device_id ids[];
+      auto err_code = clGetDeviceIDs(this,type,0,null,&num_devices);
+      assert(err_code == CL_SUCCESS);
+      ids = new cl_device_id[num_devices];
+      err_code = clGetDeviceIDs(this,type,cl_device_id.sizeof * num_devices,ids.ptr,null);
+      DeviceID device_ids[] = new DeviceID[ids.length];
+      foreach(i,id;ids) {
+        device_ids[i] = DeviceID(ids[i]);
+      }
+      return device_ids;
+    }
+  }
+  string name() {
+    return get_str_info(CL_PLATFORM_NAME);
+  }
+  string extensions() {
+    return get_str_info(CL_PLATFORM_EXTENSIONS);
+  }
+  string my_version() {
+    return get_str_info(CL_PLATFORM_VERSION);
+  }
+  string vendor() {
+    return get_str_info(CL_PLATFORM_VENDOR);
+  }
+  string profile() {
+    return get_str_info(CL_PLATFORM_PROFILE);
+  }
+  DeviceID [] all_devices() {
+    return get_devices(CL_DEVICE_TYPE_ALL);
+  }
+  DeviceID [] gpu_devices() {
+    return get_devices(CL_DEVICE_TYPE_GPU);
+  }
+  DeviceID [] cpu_devices() {
+    return get_devices(CL_DEVICE_TYPE_CPU);
+  }
+  DeviceID [] accelerator_devices() {
+    return get_devices(CL_DEVICE_TYPE_DEFAULT);
+  }
+}
+PlatformID[] get_all_platform_ids() {
+  cl_uint num_platforms;
+  auto err_code = clGetPlatformIDs(0,null,&num_platforms);
+  assert(err_code == CL_SUCCESS);
+  return get_platform_ids(num_platforms);
+}
+PlatformID[] get_platform_ids(cl_uint num_platforms) {
+  cl_platform_id cl_ids[] = new cl_platform_id[num_platforms];
+  auto err_code = clGetPlatformIDs(num_platforms,cl_ids.ptr,&num_platforms);
+  assert(err_code == CL_SUCCESS);
+  PlatformID ids[] = new PlatformID[min(num_platforms,cl_ids.length)];
+  foreach(i,id;ids) {
+    ids[i] = PlatformID(cl_ids[i]);
+  }
+  return ids;
+}
+unittest {
+  assert(get_platform_ids(1).length <= 1);
+  assert(get_platform_ids(get_all_platform_ids().length + 2).length > 0);
+}
+unittest {
+  assert(get_all_platform_ids().length > 0);
+}
