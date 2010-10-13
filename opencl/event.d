210ca8c32ae48245e4b7283aed91e578c0bdad47
Michael Chaten <mchaten@gmail.com>
2010-10-13 10:32:50 -0400

    Remove event callbacks for now as they are unstable

--- opencl/event.d
+++ opencl/event.d
@@ -3,7 +3,7 @@ import opencl.c;
 import opencl.error;
 import opencl.context;
 import opencl._get_info;
-import core.thread;
+import opencl._malloc;
 struct Event {
   cl_event _event;
   alias _event this;
@@ -25,16 +25,6 @@ struct Event {
   void set_user_event_status(cl_int execution_status) {
     throw_error(clSetUserEventStatus(this,execution_status));
   }
-  void set_callback(cl_int execution_status,void function(Event,cl_int) callback) {
-  //Wrapper around a d function to make it easier to callback
-  //This function is most likely to be called from a pthread or a windows thread, which are not
-  //Scanned by the gc.
-    throw_error(clSetEventCallback(this,execution_status,&_callback_func,callback));
-  }
-}
-extern(System) void _callback_func(cl_event event,cl_int status,void * user_data) {
-  void function(Event,cl_int) callback = cast(void function(Event,cl_int))user_data;
-  callback(Event(event),status);
 }
 void wait_for_events(const Event [] events) {
   throw_error(clWaitForEvents(events.length,(cast(const cl_event [])events).ptr));
